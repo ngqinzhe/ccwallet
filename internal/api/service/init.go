e4c78effee9d8dba9ccc3be1e5b0b64288419bb0
Ng Qin Zhe <ngqinzhe2@gmail.com>
2025-03-15 08:11:22 +0800

    fix interface

--- internal/api/service/init.go
+++ internal/api/service/init.go
@@ -10,7 +10,7 @@ type AccountService interface {
 	Deposit(ctx context.Context, req *DepositRequest) (*DepositResponse, error)
 	Withdraw(ctx context.Context, req *WithdrawRequest) (*WithdrawResponse, error)
 	Transfer(ctx context.Context, req *TransferRequest) (*TransferResponse, error)
-	GetWalletBalance(ctx context.Context, req *GetWalletBalanceRequest) (*GetTransactionHistoryResponse, error)
+	GetWalletBalance(ctx context.Context, req *GetWalletBalanceRequest) (*GetWalletBalanceResponse, error)
 	GetTransactions(ctx context.Context, req *GetTransactionHistoryRequest) (*GetTransactionHistoryResponse, error)
 }
 
